6de0362b11dbfcbb92e9ce614a30dbb5f550b9ed
Denis Stoyanouski <vandenst@tut.by>
2022-08-06 15:58:28 +0300

    Add isPalindromic method (A Palindromic number is symmetrical; in other words, it stays the same regardless of whether we read it from left or right).

--- numbers/Main.java
+++ numbers/Main.java
@@ -1,11 +1,12 @@
 package numbers;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
 
     static Scanner scanner = new Scanner(System.in);
-    final private static int buzzNumber = 7;
+    final private static BigInteger buzzNumber = new BigInteger("7");
 
     public static void main(String[] args) {
         input();
@@ -13,45 +14,60 @@ public class Main {
 
     private static void input() {
         String input;
-        int number = 0;
-        boolean naturalNumber = true;
-        //do {
-            System.out.println("Enter a natural number: ");
+        BigInteger number;
+
+        System.out.printf("Welcome to Amazing Numbers!%n%n" +
+                "Supported requests:%n%n" +
+                "- enter a natural number to know its properties;%n" +
+                "- enter 0 to exit.%n");
+
+        do {
+            System.out.print("Enter a request: ");
             input = scanner.next();
-            if(!input.matches("\\d+") || "0".equals(input)) {
-                System.out.println("This number is not natural!");
-                naturalNumber = false;
+            if(!input.matches("\\d+")) {
+                System.out.println("The first parameter should be a natural number or zero.");
+            } else if ("0".equals(input)) {
+                System.out.println("");
+                System.out.println("Goodbye!");
+                System.exit(0);
             } else {
-                number = Integer.parseInt(input);
+                number = new BigInteger(input);
                 properties(number);
             }
-        //} while (!naturalNumber);
-
+        } while (!"0".equals(input));
     }
 
-    private static void properties(int number) {
-        System.out.printf("Properties of %d%n", number);
+    private static void properties(BigInteger number) {
+        System.out.printf("Properties of %s%n", number.toString());
         System.out.println("        even: " + isEven(number));
         System.out.println("         odd: " + isOdd(number));
         System.out.println("        buzz: " + isBuzz(number));
         System.out.println("        duck: " + isDuck(number));
+        System.out.println(" palindromic: " + isPalindromic(number));
     }
 
 
-    private static boolean isOdd(int number) {
-        return number % 2 != 0;
+    private static boolean isOdd(BigInteger number) {
+        return !number.remainder(BigInteger.valueOf(2)).equals(BigInteger.ZERO);
     }
 
-    private static boolean isEven(int number) {
-        return number % 2 == 0;
+    private static boolean isEven(BigInteger number) {
+        return number.remainder(BigInteger.TWO).equals(BigInteger.ZERO);
     }
 
-    private static boolean isBuzz(int number) {
-        return number % 10 == buzzNumber || number % buzzNumber == 0 || number == buzzNumber;
-
+    private static boolean isBuzz(BigInteger number) {
+        return number.remainder(BigInteger.TEN).equals(buzzNumber) ||
+                number.remainder(buzzNumber).equals(BigInteger.ZERO) ||
+                number.equals(buzzNumber);
     }
 
-    private static boolean isDuck(int number) {
+    private static boolean isDuck(BigInteger number) {
         return String.valueOf(number).lastIndexOf('0') > 0;
+    }
+
+    private static boolean isPalindromic(BigInteger number) {
+        StringBuilder palindromic = new StringBuilder(String.valueOf(number));
+        BigInteger mirror = new BigInteger(String.valueOf(palindromic.reverse()));
+        return number.equals(mirror);
     }
 }
